56e64bf80dcc11089f020034c8e95c36b04fbe75
ddarryl789-alt <ddarryl789@gmail.com>
2026-09-19 13:51:07 +0800

    Add question 1

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,28 +1,28 @@
-using IT_ELECTIVE_2_BSIT33E3_PREFINAL_EXAM_DelaCruz_Darryl.Models;
-using Microsoft.AspNetCore.Mvc;
+    using IT_ELECTIVE_2_BSIT33E3_PREFINAL_EXAM_DelaCruz_Darryl.Models;
+    using Microsoft.AspNetCore.Mvc;
 
-namespace IT_ELECTIVE_2_BSIT33E3_PREFINAL_EXAM_DelaCruz_Darryl.Controllers
-{
-    public class HomeController : Controller
+    namespace IT_ELECTIVE_2_BSIT33E3_PREFINAL_EXAM_DelaCruz_Darryl.Controllers
     {
-        public IActionResult Index()
+        public class HomeController : Controller
         {
-            var questions = new List<Question>
+            public IActionResult Index()
             {
-                new Question
+                var questions = new List<Question>
                 {
-                    Number = 1,
-                    QuestionText = "What is the main problem solved by using a database instead of an in-memory collection?",
-                    Answer = "It allows data to persist after the application stops."
-                }
-            };
+                    new Question
+                    {
+                        Number = 1,
+                        QuestionText = "What is the main problem solved by using a database instead of an in-memory collection?",
+                        Answer = "It allows data to persist after the application stops."
+                    }
+                };
 
-            return View(questions);
-        }
+                return View(questions);
+            }
 
-        public IActionResult Privacy()
-        {
-            return View();
+            public IActionResult Privacy()
+            {
+                return View();
+            }
         }
     }
-}
